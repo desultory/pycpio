56581b0930afc2541668964b3c3278d195f33275
Zen <z@pyl.onl>
2023-11-26 22:45:19 -0600

    fixed formatting in device node printout
    Signed-off-by: Zen <z@pyl.onl>

--- src/pycpio/cpio/data.py
+++ src/pycpio/cpio/data.py
@@ -131,7 +131,7 @@ class CPIO_CharDev(CPIOData):
     Character device object
     """
     def __str__(self):
-        return f"{super().__str__()}({self.header.rdevmajor}, {self.header.rdevminor})"
+        return f"{super().__str__()}({int(self.header.rdevmajor)}, {int(self.header.rdevminor)})"
 
 
 
