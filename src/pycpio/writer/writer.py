34453640f61cf3aefdda3071a0d2bd7fe604188e
Zen <z@pyl.onl>
2024-06-06 19:27:46 -0500

    further simplify
    Signed-off-by: Zen <z@pyl.onl>

--- src/pycpio/writer/writer.py
+++ src/pycpio/writer/writer.py
@@ -37,11 +37,11 @@ class CPIOWriter:
 
     def compress(self, data):
         """ Attempts to compress the data using the specified compression type. """
-        if self.compression == 'xz' or self.compression.lower() == 'true' or self.compression is True:
+        if self.compression == 'xz' or self.compression is True:
             import lzma
             self.logger.info("XZ compressing the CPIO data, original size: %.2f MiB" % (len(data) / (2 ** 20)))
             data = lzma.compress(data, check=self.xz_crc)
-        elif self.compression not in [False, 'False', None, '', 'false']:
+        elif self.compression is not False:
             raise NotImplementedError("Compression type not supported: %s" % self.compression)
         return data
 
